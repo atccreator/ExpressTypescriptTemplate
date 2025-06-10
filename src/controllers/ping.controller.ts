683700c31736b3ff6beb365806f867e3d39a1448
atccreator <atccreator@gmail.com>
2025-06-10 08:40:45 +0530

    default async error handling by express

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,12 +1,22 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import fs from 'fs';
 
-export const pingHandler = (req :Request, res:Response) : void =>{
+export const pingHandler =  (req :Request, res:Response, next:NextFunction) =>{
     
-    console.log('request body:', req.body);// for request body
-    console.log('request query:', req.query);//for query parameters
-    console.log('request params:', req.params);// for url parameters
+    // console.log('request body:', req.body);// for request body
+    // console.log('request query:', req.query);//for query parameters
+    // console.log('request params:', req.params);// for url parameters
 
-    res.send('PONG');
+    // res.status(200).json({
+    //     message: 'pong',
+    //     success: true
+    // });
 
-}
+    fs.readFile('sample.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            next(err);
+        }
+    });
+} 
 
